20ed63eef0cb57ddbb209fca942076de82bc174e
JiapeiSun <jiapei.sun@students.fhnw.ch>
2025-04-29 23:48:12 +0200

    Update hotel.py

--- model/hotel.py
+++ model/hotel.py
@@ -1,14 +1,28 @@
-#create a hotel class
+class Hotel:
+    def __init__(self, hotel_id: int, name: str, stars: int, address: Address):
+        self.__hotel_id = hotel_id
+        self.__name = name
+        self.__stars = stars
+        self.__address = address
 
-class hotel:
-  def__init__(self, hotel_id: int, name: str, stars: int, address: str):
-    self.hotel_id = hotel_id
-    self.name = name
-    self.stars = stars
-    self.address = addess
+    @property
+    def hotel_id(self):
+        return self.__hotel_id
 
-  def__str__(self)
-    return f"Hotel[{self.hotel_id}] {self.name} - {self.stars} Sterne, Address: {self.address}"
+    @property
+    def name(self):
+        return self.__name
 
+    @property
+    def stars(self):
+        return self.__stars
 
+    @property
+    def address(self):
+        return self.__address
 
+    def __str__(self):
+        return (
+            f"Hotel[{self.__hotel_id}] {self.__name} - {self.__stars} Sterne\n"
+            f"Adresse: {self.__address}"
+        )
