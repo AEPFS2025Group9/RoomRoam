954de0ba5f335654e6accb71941760cdbecaac1e
JiapeiSun <jiapei.sun@students.fhnw.ch>
2025-04-29 21:06:42 +0200

    Update hotel.py

---
+++ model/hotel.py
@@ -0,0 +1,14 @@
+#create a hotel class
+
+class hotel:
+  def__init__(self, hotel_id: int, name: str, stars: int, address: str):
+    self.hotel_id = hotel_id
+    self.name = name
+    self.stars = stars
+    self.address = addess
+
+  def__str__(self)
+    return f"Hotel[{self.hotel_id}] {self.name} - {self.stars} Sterne, Address: {self.address}"
+
+
+
