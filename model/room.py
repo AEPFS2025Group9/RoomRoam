914a759cd0e4154f13c76954de8cf0fdd659a248
alexsuter16 <Alex.suter@students.fhnw.ch>
2025-05-12 22:18:32 +0200

    room.py

--- model/room.py
+++ model/room.py
@@ -1,15 +1,46 @@
-Base = declarative_base()
+from models.room_type import RoomType
+from models.facility import Facility
 
-class Room (Base):
-    __tablename__ = 'room'
+class Room:
+    """
+    Model Class Room
+    """
 
-    room_id = Column(Integer, primary_key=True)
-    room_no = Column(String)
-    price_per_night = Column(Float)
+    def __init__(self, id: int, hotel_id: int, room_number: str, room_type: RoomType):
+        self._id = id
+        self._hotel_id = hotel_id
+        self._room_number = room_number
+        self._room_type = room_type
+        self._facilities = []
 
-    room_type_id = Column(Integer, ForeignKey('room_type.room_type_id'))
-    hotel_id = Column(Integer, ForeignKey('hotel.hotel_id'))
+    @property
+    def id(self) -> int:
+        return self._id
 
-    room_type = relationship("RoomType", back_populates="rooms")
-    facilities = relationship("Facilities", secondary=room_facilities, back_populates="rooms")
-    bookings = relationship("Booking", back_populates="room")
+    @property
+    def hotel_id(self) -> int:
+        return self._hotel_id
+
+    @property
+    def room_number(self) -> str:
+        return self._room_number
+
+    @property
+    def room_type(self) -> RoomType:
+        return self._room_type
+
+    @property
+    def facilities(self) -> list:
+        return self._facilities
+
+    def add_facility(self, facility: Facility):
+        if facility not in self._facilities:
+            self._facilities.append(facility)
+
+    def remove_facility(self, facility: Facility):
+        if facility in self._facilities:
+            self._facilities.remove(facility)
+
+    def __str__(self):
+        fac_names = ', '.join([f.name for f in self._facilities])
+        return f"Room {self._room_number} ({self._room_type.name}) with: {fac_names}
