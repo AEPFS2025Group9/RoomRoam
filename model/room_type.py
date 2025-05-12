91582c9794a4ba18c1df1bf2fa410ccfcc6015a5
alexsuter16 <Alex.suter@students.fhnw.ch>
2025-05-12 22:21:53 +0200

    room_type.py

--- model/room_type.py
+++ model/room_type.py
@@ -1,10 +1,34 @@
-Base = declarative_base()
+class RoomType:
+    """
+   Model Class RoomType
+    """
 
-class RoomType(Base):
-    __tablename__ = 'room_type'
+    def __init__(self, id: int, name: str, description: str, max_guests: int, price: float):
+        self._id = id
+        self._name = name
+        self._description = description
+        self._max_guests = max_guests
+        self._price = price
 
-    room_type_id = Column(Integer, primary_key=True)
-    description = Column(String)
-    max_guests = Column(Integer)
+    @property
+    def id(self) -> int:
+        return self._id
 
-    rooms = relationship("Room", back_populates="room_type")
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @property
+    def max_guests(self) -> int:
+        return self._max_guests
+
+    @property
+    def price(self) -> float:
+        return self._price
+
+    def __str__(self):
+        return f"{self._name} ({self._max_guests} guests) - CHF {self._price:.2f}"
